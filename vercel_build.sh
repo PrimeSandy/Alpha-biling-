64412ec31052534e58cb7a6375960b4317f4f9ab
PrimeSandy <primesandy@github.com>
2026-04-09 16:06:47 +0530

    Fixed Vercel build: created placeholder .env for assets

--- vercel_build.sh
+++ vercel_build.sh
@@ -4,9 +4,13 @@
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
+# Create dummy .env to satisfy flutter asset requirement
+touch .env
+
 # Get dependencies and build
 flutter doctor
 flutter pub get
 flutter build web --release
+
 
 # Note: Vercel will look for the output in build/web
